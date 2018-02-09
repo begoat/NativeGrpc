bc7ec5d8c81957b6d8f50b8c47f813b45ff30280
william huang <hylbegoat@gmail.com>
2018-02-09 23:54:56 +0800

    add typescript support

---
+++ README.md
@@ -0,0 +1,2 @@
+## reference:
+#### https://medium.com/@rintoj/react-native-with-typescript-40355a90a5d7
--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,23 @@ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Shake or press menu button for dev menu'
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+interface Props {
+
+}
+
+interface State {
+
+}
+
+export default class App extends Component<Props, State> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native!TS version is welcome;
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
@@ -43,16 +50,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
